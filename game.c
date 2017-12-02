1ab9c373608dd116de66c3a4df2319a537704843
Jonathan50 <Jonathan50@users.noreply.github.com>
2017-12-02 21:19:11 +1300

    Add dog1

--- game.c
+++ game.c
@@ -5,13 +5,21 @@ static Sprite *player;
 void InitGame(void)
 {
   player = MakeSprite("player");
+  {
+    Sprite *d1, *d2;
+    srand(time(NULL));
+    d1 = MakeSprite("dog1");
+    d2 = MakeSprite("dog1");
+    d1->rect.x = rand() % WINDOW_WIDTH;
+    d1->rect.y = rand() % WINDOW_HEIGHT;
+    d2->rect.x = rand() % WINDOW_WIDTH;
+    d2->rect.y = rand() % WINDOW_HEIGHT;
+  }
 }
 
 void UpdateGame(void)
 {
-  const Uint8 *keystate;
-
-  keystate = SDL_GetKeyboardState(NULL);
+  const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
   if (keystate[SDL_SCANCODE_LEFT])
     player->rect.x = (player->rect.x - SPEED) % WINDOW_WIDTH;
